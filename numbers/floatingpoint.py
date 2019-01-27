ea73a405c2e50a13e04fb4a1969cb9bc9f49e365
oloruntola, Aruwaji <temitopetola@gmail.com>
2019-01-27 01:29:29 +0100

    floating numbers

---
+++ numbers/floatingpoint.py
@@ -0,0 +1,7 @@
+# float (floating point real values) − Also called floats, they represent real numbers and are written with a decimal point dividing the integer and fractional parts.
+
+a = 7
+b = 4
+c = a/b
+
+print(c)
