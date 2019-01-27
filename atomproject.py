a6d0d84ff1492e0f868258d255a87bf6a1ab64a0
oloruntola, Aruwaji <temitopetola@gmail.com>
2019-01-27 23:38:26 +0100

    enumerate and range function

---
+++ atomproject.py
@@ -0,0 +1,17 @@
+mylist = [1,2,3]
+for num in range (0,11,2):
+    print(num)
+list (range(0,11,2))
+
+index_count = 0
+for letter in 'abcde':
+    print('at index {} the letter is {}'.format(index_count,letter))
+    index_count += 1
+word = 'abcde'
+for item in enumerate(word):
+    print(item)
+
+mylist1 = [1,2,3]
+mylist2 = ['a','b','c']
+for item in zip (mylist1, mylist2):
+    print(item)
