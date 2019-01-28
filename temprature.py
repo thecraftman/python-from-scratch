f5a138bdea11322fe9b8803f56d008e4bae77ab6
oloruntola, Aruwaji <temitopetola@gmail.com>
2019-01-28 02:50:16 +0100

    temperature conversion

--- temprature.py
+++ temprature.py
@@ -2,3 +2,10 @@ celcius = [0,10,20,34.5]
 
 fahrenheit = [( (9/5)*temp +32) for temp in celcius]
 print(fahrenheit)
+
+## OR
+
+fahrenheit = []
+for temp in celcius:
+    fahrenheit.append(( (9/5)*temp +32))
+print(fahrenheit)
