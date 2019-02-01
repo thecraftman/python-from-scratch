79b9f15d6dd7dcf84e9583c5d1b0e1f05f65fa1b
oloruntola, Aruwaji <temitopetola@gmail.com>
2019-02-01 21:15:14 +0100

    split string function

--- splitstring.py
+++ splitstring.py
@@ -2,5 +2,3 @@ x = 'Hi this is a string'
 
 x.split('i') #This means that from the sentence 'Hi this is a string letter i wont be printed'
              # the function .split removes it
-
-print('This is a string{}'   .format ('INSERTED'))
