c23bcefb108958ed3d7fedcaaa42321dea597f3c
oloruntola, Aruwaji <temitopetola@gmail.com>
2019-02-06 02:59:13 +0100

    else:if

---
+++ statements/elif.py
@@ -0,0 +1,10 @@
+# The elif statement allows you to check multiple expressions for TRUE and
+# execute a block of code as soon as one of the conditions evaluates to TRUE.
+
+loc = 'power house'
+if loc == 'Autoshop':
+    print("cars are cool")
+elif loc == 'power house':
+    print("The powerhouse is were things happen")
+else:
+    print("I am not alone")
