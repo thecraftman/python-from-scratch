8c627f1bfb06f6af4c9f311fc182f2adc87c201e
oloruntola, Aruwaji <temitopetola@gmail.com>
2019-02-06 02:46:13 +0100

    statements

--- statements/if.py
+++ statements/if.py
@@ -1,5 +1,8 @@
 # if statement resolves to 0 or a FALSE/TRUE value.
 
+#  The else statement is an optional statement and
+#  there could be at most only one else statement following if.
+
 hungry = True
 if hungry:
     print("FEED ME")
