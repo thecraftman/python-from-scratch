19ac5a9cc60459b23b70d82c2c98bdbfcb88beee
oloruntola, Aruwaji <temitopetola@gmail.com>
2019-02-06 04:23:45 +0100

    added more examples

--- for.py
+++ for.py
@@ -38,3 +38,12 @@ len (mylist)
 
 mylist = [(1,2),(3,4),(5,6),(7,8)]
 len (mylist)
+for item in mylist:
+    print(item)
+for a in mylist:
+    print(a)
+
+#more examples
+mylist = [(1,2,3),(5,6,7),(8,9,10)]
+for item in mylist:
+    print(item)
