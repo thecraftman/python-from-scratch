450988c9832e0016b191073e5bde34c9009ec9d6
Ore-Aruwaji, Oloruntola <temitopetola@gmail.com>
2019-02-18 02:34:11 +0100

    initial

---
+++ min and max value.py
@@ -0,0 +1,12 @@
+# solutions to test questions
+
+#1
+def evens(a,b):
+    # both numbers are even
+
+    if a%2 ==0 and b%2 ==0:
+        return min(a,b)
+    else:
+        return max(a,b)
+
+      
