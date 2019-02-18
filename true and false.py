7af72a216bd6e0de8c6bae9e64fad4a52f3ce5c7
Ore-Aruwaji, Oloruntola <temitopetola@gmail.com>
2019-02-18 02:37:08 +0100

    initial

---
+++ true and false.py
@@ -0,0 +1,13 @@
+def makes_twenty(n1,n2):
+    if n1+n2 ==20:
+        return True
+    elif n1 == 20:
+        return True
+    elif n2 == 20:
+        return True
+    else:
+        return False
+
+    #or
+    return (n1+n2) ==20 or n1==20 or n2 ==20
+        
