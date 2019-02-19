f80f4ffa1c852832138a75e64201cf45fda3fcef
Ore-Aruwaji, Oloruntola <temitopetola@gmail.com>
2019-02-19 06:38:14 +0100

    initial

---
+++ lambda_expressions.py
@@ -0,0 +1,13 @@
+def square (num):
+    result = num ** 2
+    return result
+
+# based on lamdba expressions
+square(3)
+
+# definning it based on lamda expressions
+def square (num):
+
+    return num ** 2
+
+square(3)
