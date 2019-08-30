1bdc75a63874ea74ec1039d2de41713b74c1f42a
Ore-Aruwaji Tola <temitopetola@gmail.com>
2019-08-30 23:08:25 +0100

    Update splitstring.py

--- splitstring.py
+++ splitstring.py
@@ -1,4 +1,4 @@
-x = 'Hi this is a string'
+x = 'Hi this is a string
 
 x.split('i') #This means that from the sentence 'Hi this is a string letter i wont be printed'
              # the function .split removes it
