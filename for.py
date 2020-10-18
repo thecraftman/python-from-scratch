3c57e7c04773acea3d07e9eca25c25d9a42945c0
Ore-Aruwaji Tola <temitopetola@gmail.com>
2020-10-18 18:59:29 +0100

    Update for.py

--- for.py
+++ for.py
@@ -1,6 +1,5 @@
-#for loops are traditionally used
-#when you have a block of code which
-#you want to repeat a fixed number of times
+#for loops are traditionally used when you have a block of code which you want to repeat a fixed number of times.
+
 
 mylist = [1,2,3,4,5,6,7,8,9,10]
 for num in mylist:
